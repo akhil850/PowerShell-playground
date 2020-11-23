a868c9eb4d6cb94484cb67226ec2f7b4928ad43d
akhil850 <akhil850@gmail.com>
2020-11-23 18:41:56 +0530

    Updates

---
+++ Scripts/Bulk_remove.ps1
@@ -0,0 +1,21 @@
+Get-ChildItem -include "*ADIDAS*.html" -recurse -force |  Remove-Item -force
+
+Get-ChildItem -include "*commentaires*.html" -recurse -force |  Remove-Item -force
+
+Get-ChildItem -include "*Odxrebc*.html" -recurse -force |  Remove-Item -force
+
+Get-ChildItem -include "*yeezy*.html" -recurse -force |  Remove-Item -force
+
+Get-ChildItem -include "*yung-xx-x-xxxxx*.html" -recurse -force |  Remove-Item -force
+
+Get-ChildItem -include "*AMAZON--BOOST*.html" -recurse -force  |  Remove-Item -force
+
+Get-ChildItem -include "*EXPOSED-YouTube*.html" -recurse -force |  Remove-Item -force
+
+Get-ChildItem -include "*Best-Static-image*.html" -recurse -force |  Remove-Item -force
+
+Get-ChildItem -include "*Nike-Court*.html" -recurse -force |  Remove-Item -force
+
+Get-ChildItem -include "*MEN-S-SHOES*.html" -recurse -force |  Remove-Item -force
+
+Get-ChildItem -include "*AliExpress-Coupon*.html" -recurse -force |  Remove-Item -force
