be7d8b0f9fe50604421f392d676395145c62f63d
akhil850 <akhil850@gmail.com>
2021-01-19 12:20:13 +0530

    Updates

--- Scripts/Bulk_remove.ps1
+++ Scripts/Bulk_remove.ps1
@@ -1,21 +1,23 @@
-Get-ChildItem -include "*ADIDAS*.html" -recurse -force |  Remove-Item -force
+Get-ChildItem -include "*ADIDAS*.html" -recurse -force |  Remove-Item -force -Verbose
 
-Get-ChildItem -include "*commentaires*.html" -recurse -force |  Remove-Item -force
+Get-ChildItem -include "*commentaires*.html" -recurse -force |  Remove-Item -force -Verbose
 
-Get-ChildItem -include "*Odxrebc*.html" -recurse -force |  Remove-Item -force
+Get-ChildItem -include "*Odxrebc*.html" -recurse -force |  Remove-Item -force -Verbose
 
-Get-ChildItem -include "*yeezy*.html" -recurse -force |  Remove-Item -force
+Get-ChildItem -include "*yeezy*.html" -recurse -force |  Remove-Item -force -Verbose
 
-Get-ChildItem -include "*yung-xx-x-xxxxx*.html" -recurse -force |  Remove-Item -force
+Get-ChildItem -include "*yung-xx-x-xxxxx*.html" -recurse -force |  Remove-Item -force -Verbose
 
-Get-ChildItem -include "*AMAZON--BOOST*.html" -recurse -force  |  Remove-Item -force
+Get-ChildItem -include "*AMAZON--BOOST*.html" -recurse -force  |  Remove-Item -force -Verbose
 
-Get-ChildItem -include "*EXPOSED-YouTube*.html" -recurse -force |  Remove-Item -force
+Get-ChildItem -include "*EXPOSED-YouTube*.html" -recurse -force |  Remove-Item -force -Verbose
 
-Get-ChildItem -include "*Best-Static-image*.html" -recurse -force |  Remove-Item -force
+Get-ChildItem -include "*Best-Static-image*.html" -recurse -force |  Remove-Item -force -Verbose
 
-Get-ChildItem -include "*Nike-Court*.html" -recurse -force |  Remove-Item -force
+Get-ChildItem -include "*Nike-Court*.html" -recurse -force |  Remove-Item -force -Verbose
 
-Get-ChildItem -include "*MEN-S-SHOES*.html" -recurse -force |  Remove-Item -force
+Get-ChildItem -include "*MEN-S-SHOES*.html" -recurse -force |  Remove-Item -force -Verbose
 
-Get-ChildItem -include "*AliExpress-Coupon*.html" -recurse -force |  Remove-Item -force
+Get-ChildItem -include "*AliExpress-Coupon*.html" -recurse -force |  Remove-Item -force -Verbose
+
+Get-ChildItem -include "*cheap*.html" -recurse -force |  Remove-Item -force -Verbose -Verbose
